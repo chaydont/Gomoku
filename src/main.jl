3bcdcd99c7403900136382b2a2cd5ccdda4a3be1
THO_MAS <thomasgunzburger@hotmail.fr>
2019-05-08 17:36:27 +0200

    Add win conditions

--- src/main.jl
+++ src/main.jl
@@ -13,18 +13,6 @@ end
 
 get_cell_from_pixel(x, y) = Cell(div(x * 19, WINDOW_SIZE) + 1, div(y * 19, WINDOW_SIZE) + 1)
 
-function remove_forbidden(board)
-    for x in 1:19
-        for y in 1:19
-            if board[x, y] == Forbidden
-                board[x, y] == Empty
-            end
-        end
-    end
-end
-
-
-
 function play()
     board = fill(Empty, 19, 19)
     prev = 0
@@ -37,14 +25,26 @@ function play()
         cell = get_cell_from_pixel(x[1], y[1])
         display_board(board)
         if ((prev & SDL.BUTTON_LEFT) == 0) && (mouseKeys & SDL.BUTTON_LEFT) > 0
-            check_all_free_threes(board, color)
+            forbiddens = find_double_threes(board, color)
+            for cell in forbiddens
+                board[cell] = Forbidden
+            end
             if board[cell] == Empty
+                for cell in forbiddens
+                    board[cell] = Empty
+                end
                 board[cell] = color
-                is_win(board, cell, color) && break
                 check_capture(board, cell, color)
+                forbiddens = find_double_threes(board, enemy(color))
+                for cell in forbiddens
+                    board[cell] = Forbidden
+                end
+                is_win(board, color, 0) && break
+                for cell in forbiddens
+                    board[cell] = Empty
+                end
                 color = enemy(color)
             end
-            remove_forbidden(board)
         end
         prev = mouseKeys
     end
